b4bcf9e3c8df606349648a1cbaeb0cba7993a55e
charles lumba <clumba533@gmail.com>
2024-06-03 16:32:07 -0700

    commit

--- resources/views/employees/index.blade.php
+++ resources/views/employees/index.blade.php
@@ -2,45 +2,50 @@
 
 @section('content')
     <div class="container">
-        <h3 align="center" class="mt-5">Employee Management</h3>
-        <div class="row">
-            <div class="col-md-2"></div>
+        <h3 class="text-center mt-5">Employee Management</h3>
+        <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="form-area">
                     <form method="POST" action="{{ route('employees.store') }}">
                         @csrf
                         <div class="row">
-                            <div class="col-md-6">
-                                <label>Employee-First Name</label>
-                                <input type="text" class="form-control" name="first_name" required>
+                            <div class="col-md-6 mb-3">
+                                <div class="form-group">
+                                    <label for="first_name">Employee First Name</label>
+                                    <input type="text" class="form-control" id="first_name" name="first_name" required>
+                                </div>
                             </div>
-                            <div class="col-md-6">
-                                <label>Employee-Last Name</label>
-                                <input type="text" class="form-control" name="last_name" required>
+                            <div class="col-md-6 mb-3">
+                                <div class="form-group">
+                                    <label for="last_name">Employee Last Name</label>
+                                    <input type="text" class="form-control" id="last_name" name="last_name" required>
+                                </div>
                             </div>
-                            <div class="col-md-6">
-                                <label>Employee-DOB</label>
-                                <input type="date" class="form-control" name="dob" required>
+                            <div class="col-md-6 mb-3">
+                                <div class="form-group">
+                                    <label for="dob">Employee DOB</label>
+                                    <input type="date" class="form-control" id="dob" name="dob" required>
+                                </div>
                             </div>
-                            <div class="col-md-6">
-                                <label>Phone Number</label>
-                                <input type="text" class="form-control" name="phone" required>
+                            <div class="col-md-6 mb-3">
+                                <div class="form-group">
+                                    <label for="phone">Phone Number</label>
+                                    <input type="text" class="form-control" id="phone" name="phone" required>
+                                </div>
                             </div>
                         </div>
-                        <div class="row">
-                            <div class="col-md-12 mt-3">
-                                <input type="submit" class="btn btn-custom bg-yellow"  value="Register">
-                            </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-custom bg-yellow">Register</button>
                         </div>
                     </form>
                 </div>
-                
+
                 <table class="table mt-5">
-                    <thead>
+                    <thead class="thead-light">
                       <tr>
                         <th scope="col">#</th>
-                        <th scope="col">Employee First Name</th>
-                        <th scope="col">Employee Last Name</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
                         <th scope="col">DOB</th>
                         <th scope="col">Phone</th>
                         <th scope="col">Action</th>
@@ -49,21 +54,21 @@
                     <tbody>
                         @foreach ($employees as $key => $employee)
                         <tr>
-                            <td scope="col">{{ $key + 1 }}</td>
-                            <td scope="col">{{ $employee->first_name }}</td>
-                            <td scope="col">{{ $employee->last_name }}</td>
-                            <td scope="col">{{ $employee->dob }}</td>
-                            <td scope="col">{{ $employee->phone }}</td>
-                            <td scope="col">
-                            <a href="{{ route('employees.edit', $employee->id) }}">
-                                    <button class="btn btn-info btn-sm bg-pink">
-                                        <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit
-                                    </button>
+                            <td>{{ $key + 1 }}</td>
+                            <td>{{ $employee->first_name }}</td>
+                            <td>{{ $employee->last_name }}</td>
+                            <td>{{ $employee->dob }}</td>
+                            <td>{{ $employee->phone }}</td>
+                            <td>
+                                <a href="{{ route('employees.edit', $employee->id) }}" class="btn btn-info btn-sm">
+                                    <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit
                                 </a>
                                 <form action="{{ route('employees.destroy', $employee->id) }}" method="POST" style="display:inline">
                                     @csrf
                                     @method('DELETE')
-                                    <button type="submit" class="btn btn-warning btn-sm bg-red">Delete</button>
+                                    <button type="submit" class="btn btn-danger btn-sm">
+                                        <i class="" aria-hidden="true"></i> Delete
+                                    </button>
                                 </form>
                             </td>
                         </tr>
@@ -80,18 +85,17 @@
         .form-area {
             padding: 20px;
             margin-top: 20px;
-            background-color: #FFFF00;
+            background-color: #ffffcc;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0,0,0,0.1);
         }
-
-        .bi-trash-fill {
-            color: red;
-            font-size: 18px;
+        .btn-custom {
+            background-color: #ffc107;
+            color: #fff;
+            border: none;
         }
-
-        .bi-pencil {
-            color: green;
-            font-size: 18px;
-            margin-left: 20px;
+        .btn-custom:hover {
+            background-color: #e0a800;
         }
     </style>
 @endpush
